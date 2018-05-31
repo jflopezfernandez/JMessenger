0abb013bf406ddbadb417b4765596b621a06c460
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-05-31 18:17:41 -0400

    Fixed minor spacing issues

--- src/messenger/Messenger.java
+++ src/messenger/Messenger.java
@@ -4,19 +4,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package messenger;
 
 /**
  *
  * @author jflop
  */
+
 public class Messenger {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO: code application logic here
     }
     
 }
